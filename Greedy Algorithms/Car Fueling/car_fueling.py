ebcc6a4768fb8573bbecdfe4bc10e37c94c9f9b4
PRSMendis <38640848+PRSMendis@users.noreply.github.com>
2020-04-13 16:25:40 +1000

    - Working on greeding algorithms -- Money change X -- Car Fueling - (inprogress)

--- Greedy Algorithms/Car Fueling/car_fueling.py
+++ Greedy Algorithms/Car Fueling/car_fueling.py
@@ -7,14 +7,54 @@ def compute_min_number_of_refills(d, m, stops):
     assert 1 <= len(stops) <= 300
     assert 0 < stops[0] and all(stops[i] < stops[i + 1] for i in range(len(stops) - 1)) and stops[-1] < d
 
-    type here
+    # type here
+    # print("hello")
+    # print(d,m, stops)
+    start = 0
+    position = 0
+    fuel = m
+    refills = 0
+    stops.append(d)
+
+    # While we have not reached the destination
+    while position != d:
+        # Find the greatest distance that we can travel, and go there and refill our fuel
+        greatest_pos = position
+        for i in range(0,len(stops)):
+
+            if fuel + position >= stops[i]:
+                if stops[i] > greatest_pos:
+                    greatest_pos = stops[i]
+                    print(greatest_pos)
+            elif fuel + position < stops[i]:
+                refills += 1
+                position = greatest_pos
+
+                break
+            elif greatest_pos == d:
+                refills +=1
+                position = greatest_pos
+                break
+    return refills
+
+
+
+            # else:
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
-    input_d = int(input())
-    input_m = int(input())
-    input_n = int(input())
-    input_stops = list(map(int, input().split()))
-    assert len(input_stops) == input_n
+    # input_d = int(input())
+    # input_m = int(input())
+    # input_n = int(input())
+    # input_stops = list(map(int, input().split()))
+    # assert len(input_stops) == input_n
 
-    print(compute_min_number_of_refills(input_d, input_m, input_stops))
+    # print(compute_min_number_of_refills(input_d, input_m, input_stops))
+    (d, m, stops ) = (950, 400, [200, 375, 550, 750])
+    print(compute_min_number_of_refills(d, m, stops))
--- Greedy Algorithms/Money Change/money_change.py
+++ Greedy Algorithms/Money Change/money_change.py
@@ -3,9 +3,28 @@
 
 def money_change(money):
     assert 0 <= money <= 10 ** 3
-    type here
+    # type here
+    count = 0
+    c1, c2 , c3 = 10 ,5 ,1
+    while money != 0:
+        if money // c1 > 0:
+            count += money // c1
+            money -= (c1 * (money // c1))
+
+        elif money // c2 > 0:
+            count += money // c2
+            money -= (c2 * (money // c2))
+
+        elif money // c3 > 0:
+            count += money // c3
+            money -= (c3 * (money // c3))
+
+    return count
+
 
 
 if __name__ == '__main__':
     input_money = int(input())
     print(money_change(input_money))
+    # print(money_change(87))
+    # print(80 // 7, 80 % 7)

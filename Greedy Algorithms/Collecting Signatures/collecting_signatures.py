60636b5298685a8834c60f924b31eb25e81afb5f
PRSMendis <38640848+PRSMendis@users.noreply.github.com>
2020-04-23 09:51:56 +1000

    - Working on greeding algorithms -- completed collecting_signatures.ipynb

--- Greedy Algorithms/Collecting Signatures/collecting_signatures.py
+++ Greedy Algorithms/Collecting Signatures/collecting_signatures.py
@@ -1,19 +1,89 @@
 # python3
 
 from collections import namedtuple
+import collections
 from sys import stdin
+import numpy as np
 
 Segment = namedtuple('Segment', 'start end')
 
 
 def compute_optimal_points(segments):
-    type here
+    def l_dict(M):
+        d = {}
+        for i, l in enumerate(M):
+            #         print(i, l)
+            for j, num in enumerate(l):
+                #                 print(j, num)
+                if num not in d:
+                    d[num] = 1
+                else:
+                    d[num] += 1
+        return {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)}
+
+    #     print("Hello World")
+
+    # print(len(segments))
+    #     M = [[]* len(segments)]
+    M = [[] for _ in range(0, len(segments))]
+    #     print(M[0])
+
+    # e
+    for i, segment in enumerate(segments):
+        M[i] = [num for num in range(segment.start, segment.end + 1)]
+
+    #     return M
+    l_d = l_dict(M)
+    l_l = list(l_d)
+    #     return l_dict(M)
+
+    ############# with the sorted dictionary and list, read from it and use it to delete any list with the variable #####################
+    val = l_l[0]
+
+    def drow(M, v, count=0):
+        i = 0
+        #         print(len(M))
+        while i < len(M):
+            #             print("M,i = " + str(M),i)
+            for j, num in enumerate(M[i]):
+                #                 print(i, num,v)
+                if num == v:
+                    #                     print("YA")
+                    del M[i]
+                    i -= 1
+            #                 count+=1
+            i += 1
+
+        return M, count
+
+    #     return drow(M,val) == []
+    k = 0
+    v = []
+    count = 0
+    while M != []:
+        l = list(l_dict(M))[0]
+        v.append(l)
+        M, count = drow(M, v[k], count)
+        k += 1
+    return k
+
+
+
 
 
 if __name__ == '__main__':
-    n, *data = map(int, stdin.read().split())
-    input_segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
-    assert n == len(input_segments)
-    output_points = compute_optimal_points(input_segments)
-    print(len(output_points))
-    print(*output_points)
+    # n, *data = map(int, stdin.read().split())
+    # input_segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
+    # assert n == len(input_segments)
+
+    s = ([Segment(1, 3), Segment(2, 5), Segment(3, 6)])
+
+    output_points = compute_optimal_points(s)
+    # output_points = compute_optimal_points(input_segments)
+    print(output_points)
+
+
+
+
+    # print(len(output_points))
+    # print(*output_points)
--- Greedy Algorithms/Maximum Advertisement Revenue/maximum_ad_revenue.py
+++ Greedy Algorithms/Maximum Advertisement Revenue/maximum_ad_revenue.py
@@ -1,6 +1,9 @@
 # python3
 
 from itertools import permutations
+from itertools import product
+from functools import reduce
+import operator
 
 
 def max_dot_product_naive(first_sequence, second_sequence):
@@ -23,12 +26,34 @@ def max_dot_product(first_sequence, second_sequence):
     assert all(0 <= f <= 10 ** 5 for f in first_sequence)
     assert all(0 <= s <= 10 ** 5 for s in second_sequence)
 
-    type here
+    print("type here")
+    def prod(iterable):
+        return reduce(operator.mul, iterable, 1)
+
+    first_sequence.sort(reverse = True)
+    second_sequence.sort(reverse = True)
+    # print(first_sequence, second_sequence)
+    a = list(zip(first_sequence, second_sequence))
+    sum = 0
+    for n in a:
+        # print(n)
+        # print(prod(n))
+        sum += prod(n)
+    # print(a)
+    return sum
+
+
+
+
+    # return ""
 
 
 if __name__ == '__main__':
-    n = int(input())
-    prices = list(map(int, input().split()))
-    clicks = list(map(int, input().split()))
-    assert len(prices) == len(clicks) == n
+    # n = int(input())
+    # prices = list(map(int, input().split()))
+    # clicks = list(map(int, input().split()))
+    # assert len(prices) == len(clicks) == n
+
+    prices, clicks = [2,3,5], [10,20,30]
     print(max_dot_product(prices, clicks))
+    # print(prices)
